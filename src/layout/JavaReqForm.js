98a64eab47dd8971b351bfbe1b9cd089946a7629
KimDamin <brlight_dm@naver.com>
2022-06-21 20:18:05 +0900

    2022-06-21 화요일

--- src/layout/JavaReqForm.js
+++ src/layout/JavaReqForm.js
@@ -41,7 +41,7 @@ function JavaReqForm() {
                     <span>파일 첨부</span>
                     
                     <div className="ft-file-box">
-                        <input type="file" id="ft-file-input" className="dn"/>
+                        <input type="file" id="ft-file-input" className=""/>
                         <label htmlFor="ft-file-input">
                         <FontAwesomeIcon icon={faFileCode} />
                             <p>파일을 찾아 선택하세요.</p>
